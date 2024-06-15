3e05b9a009baabeb9f0a76aa8808d1d5c6ee7081
Sithum39 <115446245+Sithum39@users.noreply.github.com>
2024-06-15 12:12:48 +0530

    link update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,40 +8,40 @@ function Footer() {
       
     return (
         <footer className="footer-distributed">
-            <div className="grid grid-cols-2 gap-0  font-krona">
-                <div className=' text-white bg-gray-900 p-4 md:px-4'>
-                    <p className='pr-8 pl-8 text-base text-center md:text-3xl md:ml-8 '>Organized By</p>
+            <div className="grid grid-cols-2 gap-0 font-krona">
+                <div className='p-4 text-white bg-gray-900  md:px-4'>
+                    <p className='pl-8 pr-8 text-base text-center md:text-3xl md:ml-8 '>Organized By</p>
                 </div>
-                <div className=' text-white bg-gray-900 p-4 md:px-4 '>
-                    <p className='pr-0 pl-16 text-base md:text-3xl sm:ml-10 lg:ml-40 xl:ml-60 2xl:ml-80 '>Follow Us On</p>
+                <div className='p-4 text-white bg-gray-900  md:px-4'>
+                    <p className='pl-16 pr-0 text-base md:text-3xl sm:ml-10 lg:ml-40 xl:ml-60 2xl:ml-80 '>Follow Us On</p>
                 </div>
             </div>
     
             <div className="footer-left">
                 <div className="grid grid-cols-3 gap-0">
                     <div className='flex justify-center md:justify-start'>
-                        <img src="club_logo.png" alt="1" className='w-4/6 md:w-1/3 ml-12 md:ml-44 mt-4'/>
+                        <img src="club_logo.png" alt="1" className='w-4/6 mt-4 ml-12 md:w-1/3 md:ml-44'/>
                     </div>
                     <div className='flex justify-center md:justify-start'>
-                        <h3 className=' ml-2  xl:-ml-24 mt-8 xl:mt-20 text-white text-lg lg:text-2xl text-center md:text-left '>
+                        <h3 className='mt-8 ml-2 text-lg text-center text-white  xl:-ml-24 xl:mt-20 lg:text-2xl md:text-left'>
                             Electronics and Computer<br/>
                             <span>Science Club</span>
                         </h3>
                     </div>
-                    <div className='lg:grid grid-cols-3 gap-0 mx-12 place-content-start mt-8 md:ml-6 '>
+                    <div className='grid-cols-3 gap-0 mx-12 mt-8 lg:grid place-content-start md:ml-6 '>
                         <a href="https://web.facebook.com/UOKRobotBattles">
                             <IoLogoFacebook className='w-10 h-10 md:w-16 md:h-16 lg:mx-8 text-white/80'/>
                         </a>
                         <a href="https://www.linkedin.com/company/uokrobotbattles/posts/?feedView=all">
                             <ImLinkedin className='w-9 h-9 md:w-14 md:h-14 lg:mx-4 text-white/80'/>
                         </a>
-                        <a href="https://www.youtube.com/@ECSCUOK">
-                            <ImYoutube className='w-10 h-10 md:w-14 md:h-14 lg:mx-0 xl:-mx-4  text-white/80'/>
+                        <a href="https://youtube.com/@ecscuok?si=tVpud5BPZpfJB3lt">
+                            <ImYoutube className='w-10 h-10 md:w-14 md:h-14 lg:mx-0 xl:-mx-4 text-white/80'/>
                         </a>
                     </div>
                 </div>
             </div>
-            <p className='text-gray-400 text-xs md:text-sm font-normal text-center mb-8 mt-5 md:mb-9'>
+            <p className='mt-5 mb-8 text-xs font-normal text-center text-gray-400 md:text-sm md:mb-9'>
                 © 2024 All rights reserved
             </p>
         </footer>
