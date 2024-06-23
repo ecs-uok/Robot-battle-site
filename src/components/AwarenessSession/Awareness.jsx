a285eb993c0ae7680a7f7c6ad48c43313efbcaec
Sithum39 <115446245+Sithum39@users.noreply.github.com>
2024-06-24 01:00:00 +0530

    links update

--- src/components/AwarenessSession/Awareness.jsx
+++ src/components/AwarenessSession/Awareness.jsx
@@ -8,15 +8,15 @@ const Awareness = () => {
     md:text-start
     text-center"
     >
-      <h1 className="text-white font-krona text-3xl   md:text-4xl ">
+      <h1 className="text-3xl text-white font-krona md:text-4xl ">
         AWARENES SESSION
         
       </h1>
       
-      <div className="flex mt-5 flex-col md:flex-row  items-center gap-10 justify-between md:mt-10">
+      <div className="flex flex-col items-center justify-between gap-10 mt-5 md:flex-row md:mt-10">
         <div className="flex flex-col gap-10 md:flex md:flex-row md:justify-between ">
-          <div className="md:flex md:flex-col gap-8">
-            <p className=" text-white font-poppins text-justify-end opacity-90 text-xl space-y-4 md:text-start mt-5 font-normal leading-relaxed  md:text-xl text-center ">
+          <div className="gap-8 md:flex md:flex-col">
+            <p className="mt-5 space-y-4 text-xl font-normal leading-relaxed text-center text-white  font-poppins text-justify-end opacity-90 md:text-start md:text-xl">
               Tune into this insightful video, where you'll discover
               professional strategies and essential guidelines for excelling in
               robot battle competitions, presented by Dr. Kasun Piyumal. The
@@ -25,14 +25,14 @@ const Awareness = () => {
               according to the latest rules and standards.
             </p>
             {/* <a href="https://drive.google.com/file/d/12-g5isYXt1Hpny74pIzvhhKUxRroL5XZ/view?usp=drive_link">
-              <button className=" bg-gradient-to-r from-pink-500 border-pink-400 border-2 to-fuchsia-800 text-lg text-white font-poppins px-4 py-2 rounded-full mt-8">
+              <button className="px-4 py-2 mt-8 text-lg text-white border-2 border-pink-400 rounded-full  bg-gradient-to-r from-pink-500 to-fuchsia-800 font-poppins">
                 Watch Now
               </button>{" "}
             </a> */}
             <div className="mt-10">
               <Button
                 text="Watch Now"
-                url="https://drive.google.com/file/d/12-g5isYXt1Hpny74pIzvhhKUxRroL5XZ/view?usp=drive_link"
+                url="https://youtu.be/sidEDf2QvTQ?si=S79kt2GtBsXAkaoF"
               />{" "}
             </div>
           </div>
